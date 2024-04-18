b9bb3569b7fc3179829101deffb841f1d684f415
João Fonseca <jvl10fonseca@gmail.com>
2024-04-18 13:08:23 -0300

    first commit

---
+++ api_core/data.php
@@ -0,0 +1,84 @@
+<?php   
+
+return [    
+    [
+        'name' => 'João',
+        'email' => 'joao@gmail.com'
+    ],
+    [
+        'name' => 'Maria',
+        'email' => ''
+    ],
+    [
+        'name' => 'Carlos',
+        'email' => 'carlos@teste.com'
+    ],
+    [
+        'name' => 'Ana',
+        'email' => 'anatesla.com'
+    ],
+    [
+        'name' => 'Pedro',
+        'email' => 'pedro@gmail.com'
+    ],
+    [
+        'name' => 'Juliana',
+        'email' => 'juliana@example.com'
+    ],
+    [
+        'name' => 'Lucas',
+        'email' => 'lucas@hotmail.com'
+    ],
+    [
+        'name' => 'Mariana',
+        'email' => 'mariana@example.com'
+    ],
+    [
+        'name' => 'Gustavo',
+        'email' => 'gustavo@gmail.com'
+    ],
+    [
+        'name' => 'Beatriz',
+        'email' => 'beatriz@hotmail.com'
+    ],
+    [
+        'name' => 'Fernando',
+        'email' => 'fernando@example.com'
+    ],
+    [
+        'name' => 'Camila',
+        'email' => 'camila@gmail.com'
+    ],
+    [
+        'name' => 'Rafael',
+        'email' => 'rafael@example.com'
+    ],
+    [
+        'name' => 'Isabela',
+        'email' => 'isabela@hotmail.com'
+    ],
+    [
+        'name' => 'Daniel',
+        'email' => 'daniel@gmail.com'
+    ],
+    [
+        'name' => 'Patrícia',
+        'email' => 'patricia@example.com'
+    ],
+    [
+        'name' => 'Gabriel',
+        'email' => 'gabriel@gmail.com'
+    ],
+    [
+        'name' => 'Laura',
+        'email' => 'laura@example.com'
+    ],
+    [
+        'name' => 'Diego',
+        'email' => ''
+    ],
+    [
+        'name' => 'Tatiana',
+        'email' => 'tatiana@example.com'
+    ]
+];
---
+++ api_core/response.php
@@ -0,0 +1,30 @@
+<?php
+
+class Response
+{
+    public static function json($status= 200, $message= 'success', $data = null) 
+    {
+
+        header('Content-Type: application/json');
+        //check if api is active
+        if(!API_IS_ACTIVE){
+            return json_encode([
+                'status' => 400,
+                'message' => 'API Is not running',
+                'api_version' => API_VERSION,
+                'time_response' => time(),
+                'datetime_response' => date('Y-m-d H:i:s'),
+                'data' => null
+            ], JSON_PRETTY_PRINT);
+        }
+        
+        return json_encode([
+            'status' => $status,
+            'message' => $message,
+            'api_version' => API_VERSION,
+            'time_response' => time(),
+            'datetime_response' => date('Y-m-d H:i:s'),
+            'data' => $data
+        ], JSON_PRETTY_PRINT);
+    }
+}
